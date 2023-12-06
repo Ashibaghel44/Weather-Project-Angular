6fcaf9bfb0379d9721f53c3ef8a977e0c30174d8
Ashibaghel44 <138962968+Ashibaghel44@users.noreply.github.com>
2023-12-06 16:24:53 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { subscribe } from 'diagnostics_channel';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weather-App';
+  error: any;
+
+  constructor(private http: HttpClient) {
+
+
+
+  }
+  weatherData: any;
+  city = '';
+  
+
+  check() {
+  this.weatherData = '';
+  let appId = 'bfa9676b5d906c86dc05a9ccc704e8a1';
+  let url ='https://api.openweathermap.org/data/2.5/weather?q'
+  let params = {q:this.city, appid:appId }
+    this.http.get(url,{params}).subscribe(
+      (data)=>{
+        this.weatherData = data
+
+    },(error)=>{
+      error= 'data not'
+    })
+  }
+
 }
